d1a4f710f0f116f2a97242a797f97eacca258870
p1ck23 <danil-vetrov47@mail.ru>
2024-05-06 01:14:59 +0400

    edit and refact endgame

--- src/components/EndGameModal/EndGameModal.jsx
+++ src/components/EndGameModal/EndGameModal.jsx
@@ -4,63 +4,82 @@ import { Button } from "../Button/Button";
 
 import deadImageUrl from "./images/dead.png";
 import celebrationImageUrl from "./images/celebration.png";
+import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import { addLeader } from "../../api";
 
-export function EndGameModal({
-  isWon,
-  gameDurationSeconds,
-  gameDurationMinutes,
-  onClick,
-  isLeader,
-  isOpenAllCards,
-  isEasyMode,
-}) {
-  const [userName, setUserName] = useState("");
-  const [isResultSent, setIsResultSent] = useState(false);
-  const title = isLeader ? "Вы попали\n на лидерборд" : isWon ? "Вы победили!" : "Вы проиграли!";
+export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, onClick, withoutSuperpowers }) {
+  const [username, setUsername] = useState("Пользователь");
+  const [isFinishedAddingToLeaderboard, setIsFinishedAddingToLeaderboard] = useState(false);
+  const buttonRef = useRef();
+
+  const time = gameDurationMinutes * 60 + gameDurationSeconds;
+  const currentLevel = useSelector(state => state.game.currentLevel);
+  const isActiveEasyMode = useSelector(state => state.game.isActiveEasyMode);
+  const leaders = useSelector(state => state.game.leaders);
+
+  const isLeader = leaders.filter(leader => {
+    return leader.time > time;
+  });
+
+  function isAddToLeaders() {
+    if (isWon === true && isLeader.length > 0 && currentLevel === 9) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function achievements() {
+    if (isActiveEasyMode === false && withoutSuperpowers === true) {
+      return [1, 2];
+    } else if (isActiveEasyMode === false && withoutSuperpowers === false) {
+      return [1];
+    } else if (isActiveEasyMode === true && withoutSuperpowers === true) {
+      return [2];
+    } else {
+      return [];
+    }
+  }
+
+  function addToLeaderboard({ username, time, achievements }) {
+    buttonRef.disabled = true;
+
+    addLeader({ username, time, achievements }).then(() => {
+      buttonRef.disabled = false;
+      setIsFinishedAddingToLeaderboard(true);
+      setUsername("");
+    });
+  }
+
+  const title = isWon ? (isAddToLeaders() === true ? "Вы попали на Лидерборд!" : "Вы победили!") : "Вы проиграли!";
 
   const imgSrc = isWon ? celebrationImageUrl : deadImageUrl;
 
   const imgAlt = isWon ? "celebration emodji" : "dead emodji";
-  const handleClick = () => {
-    if (!userName.trim()) {
-      alert("Вы ввели пустую строку");
-      return;
-    }
-    const time = gameDurationMinutes * 60 + gameDurationSeconds;
-    const achievements = getAchievements();
-    addLeader({ name: userName, time, achievements }).then(() => {
-      setIsResultSent(true);
-      alert("Ресультат отправлен");
-    });
-  };
-  function getAchievements() {
-    const achievements = [1, 2];
-    if (isOpenAllCards) {
-      achievements.shift();
-    }
-    if (isEasyMode) {
-      achievements.pop();
-    }
-    return achievements;
-  }
+
   return (
     <div className={styles.modal}>
       <img className={styles.image} src={imgSrc} alt={imgAlt} />
       <h2 className={styles.title}>{title}</h2>
-      {isLeader && !isResultSent && (
-        <div className={styles.boxLeaderButtonInput}>
+      {isAddToLeaders() === true && isFinishedAddingToLeaderboard === false && (
+        <>
           <input
-            className={styles.inputLeaderBoard}
+            className={styles.username}
             type="text"
             placeholder="Пользователь"
-            value={userName}
-            onChange={e => setUserName(e.target.value)}
+            value={username}
+            onChange={event => setUsername(event.target.value)}
           />
-          <Button onClick={handleClick}>Отправить</Button>
-        </div>
+          <button
+            className={styles.addButton}
+            ref={buttonRef}
+            onClick={() => addToLeaderboard({ username, time, achievements })}
+          >
+            Отправить
+          </button>
+        </>
       )}
       <p className={styles.description}>Затраченное время:</p>
       <div className={styles.time}>
@@ -68,7 +87,8 @@ export function EndGameModal({
       </div>
 
       <Button onClick={onClick}>Начать сначала</Button>
-      <Link className={styles.backGameLeader} to="/Leaderboard">
+
+      <Link className={styles.leaderboardLink} to="/leaderboard">
         Перейти к лидерборду
       </Link>
     </div>
